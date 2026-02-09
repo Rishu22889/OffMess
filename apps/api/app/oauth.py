f4eb4dcc37cfb5c2039fd178df21e0586217c73f
Rishu <rk1258106@gmail.com>
2026-02-10 00:47:10 +0530

    fix: Use settings for OAuth config instead of .env file
    - This allows environment variables to work on Render
    - OAuth will now read from Render environment variables

--- apps/api/app/oauth.py
+++ apps/api/app/oauth.py
@@ -1,15 +1,12 @@
 from authlib.integrations.starlette_client import OAuth
-from starlette.config import Config
-
-# Load config
-config = Config(".env")
+from .config import settings
 
 oauth = OAuth()
 
 oauth.register(
     name='google',
-    client_id=config('GOOGLE_CLIENT_ID'),
-    client_secret=config('GOOGLE_CLIENT_SECRET'),
+    client_id=settings.google_client_id,
+    client_secret=settings.google_client_secret,
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
